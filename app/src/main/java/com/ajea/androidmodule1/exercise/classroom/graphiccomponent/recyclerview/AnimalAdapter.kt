0c67501a238ac94fa4d8e8e9f55d4e8b8c5f821f
Alan Espinosa <>
2025-02-18 12:31:20 -0600

    Update adapter to use view binding

--- app/src/main/java/com/ajea/androidmodule1/exercise/classroom/graphiccomponent/recyclerview/AnimalAdapter.kt
+++ app/src/main/java/com/ajea/androidmodule1/exercise/classroom/graphiccomponent/recyclerview/AnimalAdapter.kt
@@ -1,30 +1,23 @@
 package com.ajea.androidmodule1.exercise.classroom.graphiccomponent.recyclerview
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.ajea.androidmodule1.R
+import com.ajea.androidmodule1.databinding.ItemAnimalBinding
 import com.squareup.picasso.Picasso
 
 class AnimalAdapter(val list: List<AnimalEntity>) : RecyclerView.Adapter<AnimalAdapter.AnimalViewHolder>() {
 
     var onItemSelected : ((AnimalEntity) -> Unit)? = null
 
-    class AnimalViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val tvName = view.findViewById<TextView>(R.id.tvName)
-        val tvColor = view.findViewById<TextView>(R.id.tvColor)
-        val card = view.findViewById<CardView>(R.id.cardAnimal)
-        val ivAnimal = view.findViewById<ImageView>(R.id.ivAnimal)
+    class AnimalViewHolder(val binding : ItemAnimalBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun render(animalEntity: AnimalEntity, onItemSelected : ((AnimalEntity) -> Unit)?){
-            tvName.text = animalEntity.name
-            tvColor.text = animalEntity.color
+            binding.tvName.text = animalEntity.name
+            binding.tvColor.text = animalEntity.color
 
-            card.setOnClickListener {
+            binding.cardAnimal.setOnClickListener {
                 onItemSelected?.invoke(animalEntity)
             }
 
@@ -32,13 +25,13 @@ class AnimalAdapter(val list: List<AnimalEntity>) : RecyclerView.Adapter<AnimalA
                 .load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM1-2B5wjpFuyYrSCslCd0do7Do5-wcCwnOQ&usqp=CAU")
                 .placeholder(R.drawable.img_placeholder)
                 .error(R.drawable.img_android)
-                .into(ivAnimal)
+                .into(binding.ivAnimal)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_animal, parent, false)
-        return AnimalViewHolder(view)
+        val binding = ItemAnimalBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return AnimalViewHolder(binding)
     }
 
     override fun getItemCount(): Int = list.size
